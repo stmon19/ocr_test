86ceb054239de4a66a6740f32269707ac83b5471
stmon19 <stormy.monday.blues19@gmail.com>
2020-07-08 15:22:06 +0900

    tesseractサンプルの実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
-import 'package:file_picker/file_picker.dart';
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:flutter/services.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'dart:async';
+
 import 'package:tesseract_ocr/tesseract_ocr.dart';
 
 void main() => runApp(MyApp());
@@ -11,9 +18,45 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  bool _scanning = false;
-  String _extractText = '';
-  int _scanTime = 0;
+  String _extractText = 'Unknown';
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    String extractText;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      final Directory directory = await getTemporaryDirectory();
+      final String imagePath = join(
+        directory.path,
+        "tmp_1.jpg",
+      );
+      final ByteData data = await rootBundle.load('images/test_e.png');
+      final Uint8List bytes = data.buffer.asUint8List(
+        data.offsetInBytes,
+        data.lengthInBytes,
+      );
+      await File(imagePath).writeAsBytes(bytes);
+
+      extractText = await TesseractOcr.extractText(imagePath, language: 'jpn');
+    } on PlatformException {
+      extractText = 'Failed to extract text';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _extractText = extractText;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,50 +65,13 @@ class _MyAppState extends State<MyApp> {
           appBar: AppBar(
             title: const Text('Tesseract OCR'),
           ),
-          body: Container(
-            padding: EdgeInsets.all(16),
-            child: ListView(
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    RaisedButton(
-                      child: Text('Select image'),
-                      onPressed: () async {
-                        var file =
-                            await FilePicker.getFilePath(type: FileType.image);
-                        _scanning = true;
-                        setState(() {});
-
-                        var watch = Stopwatch()..start();
-                        _extractText = await TesseractOcr.extractText(file);
-                        _scanTime = watch.elapsedMilliseconds;
-
-                        _scanning = false;
-                        setState(() {});
-                      },
-                    ),
-                    // It doesn't spin, because scanning hangs thread for now
-                    _scanning
-                        ? SpinKitCircle(
-                            color: Colors.black,
-                          )
-                        : Icon(Icons.done),
-                  ],
-                ),
-                SizedBox(
-                  height: 16,
-                ),
-                Text(
-                  'Scanning took $_scanTime ms',
-                  style: TextStyle(color: Colors.red),
-                ),
-                SizedBox(
-                  height: 16,
-                ),
-                Center(child: SelectableText(_extractText)),
-              ],
-            ),
+          body: Column(
+            children: <Widget>[
+              Center(
+                child: Text('Detected Text: $_extractText\n'),
+              ),
+              Image.asset('images/test_e.png', height: 300.0),
+            ],
           )),
     );
   }
